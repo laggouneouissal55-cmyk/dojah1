0d664510965a143a08b4bf06a03571a91e001b88
laggouneouissal55-cmyk <laggouneouissal55@gmail.com>
2026-01-07 23:09:38 +0100

    Add files via upload

---
+++ MonProjet.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Démarrage de mon projet de recherche...
+start "" "C:\Program Files\Zotero\Zotero.exe"
+start "" "C:\Program Files\LibreOffice\program\swriter.exe"
+start chrome "https://github.com/laggouneouissal55-cmyk/dojah1"
+pause
